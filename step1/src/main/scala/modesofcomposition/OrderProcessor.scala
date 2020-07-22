8e996dc57d0074ae820de2e623526c44dbeee3db
Bharg Sharma <bharg.sharma@rea-group.com>
2020-07-22 17:52:09 +1000

    Step 1 and Step 2 done

--- step1/src/main/scala/modesofcomposition/OrderProcessor.scala
+++ step1/src/main/scala/modesofcomposition/OrderProcessor.scala
@@ -3,7 +3,11 @@ package modesofcomposition
 object OrderProcessor {
 
   def decodeMsg[F[_]: ApplicativeError[*[_], Throwable]](msg: Array[Byte]): F[OrderMsg] =
-    ???
+    errorValueFromEither[F](parser.decode[OrderMsg](new String(msg)))
+//  errorValueFromEither contructs a new ErrorValueFromEitherPartiallyApplied with a effectful type F[_]
+//  the apply method of ErrorValueFromEitherPartiallyApplied takes an e and implicit ApplicativeError
+//  e: => Either[E,A] which is the return type of decode: Either[Error, A]
+//  and we have an implicit ApplicativeError on our method
 
 }
 
--- step2/src/main/scala/modesofcomposition/OrderProcessor.scala
+++ step2/src/main/scala/modesofcomposition/OrderProcessor.scala
@@ -6,11 +6,25 @@ object OrderProcessor {
   def resolveOrderMsg[F[_]: Sync: Parallel: SkuLookup: CustomerLookup](msg: OrderMsg): F[CustomerOrder] =
     msg match { case OrderMsg(custIdStr, items) =>
 
-      val resolveCustomer: F[Customer] = ???
+      val resolveCustomer: F[Customer] = {
+        val fofEitherStrOrCust: F[Either[String, Customer]] = CustomerLookup[F].resolveCustomerId(custIdStr)
+        val fOfCustomer: F[Customer] = fofEitherStrOrCust.flatMap(eitherStringOrCust => errorValueFromEither[F](eitherStringOrCust))
+        fOfCustomer
+      }
 
-      val resolveSkuQuantity: ((String, Int)) => F[SkuQuantity] = ???
+      val resolveSkuQuantity: ((String, Int)) => F[SkuQuantity] = {
+        case (skuCode, quantity) => {
+          val fOfSku: F[Sku] = SkuLookup[F].resolveSku(skuCode).flatMap(eitherStrOrSku => errorValueFromEither[F](eitherStrOrSku))
+          val fOfPosInt: F[PosInt] = PosInt.fromF[F](quantity) // same as F[Refined[PosInt, Int]]
+//          (fOfSku, fOfPosInt).mapN(SkuQuantity) this works but is not resolved in parallel, use parMapN
+          (fOfSku, fOfPosInt).parMapN(SkuQuantity)
+        }
+      }
 
-      val resolveSkus: F[NonEmptyChain[SkuQuantity]] = ???
+      val resolveSkus: F[NonEmptyChain[SkuQuantity]] = {
+//        items.map(tuple => resolveSkuQuantity(tuple)).sequence same as doing traverse, works but also not in parallel
+        items.parTraverse(resolveSkuQuantity)
+      }
 
       //applicative composition
       (
